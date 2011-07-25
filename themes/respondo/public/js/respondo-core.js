763394fe454f2c44c2c8f15993b7e8505ff776bd
dale tan <wtdtan@gmail.com>
2011-07-25 17:29:28 -0400

    added a little structure, confirm "delete" before deleting

--- themes/respondo/public/js/respondo-core.js
+++ themes/respondo/public/js/respondo-core.js
@@ -1,31 +1,51 @@
 // i don't feel like abstracting anything right now
 
+var Respondo = {};
+
+
 $(function () {
   
-  var isClosing = false;
-  
-  function closeLangs(cb) {
-    isClosing = true;
-    $('.language-wrap pre').each(function (i, a) {
-      var $a = $(a);
-      $a.animate({ height: 0 }, 'fast', 'easeOutQuad', function () {
-        $a.hide();
-        isClosing = false;
-        cb();
-      });
-    });
-  }
-  
-  $('.language-wrap h3').each(function (i, a) { 
-    var $a = $(a);
-    $a.click(function () {
-      if (!isClosing) {
-        closeLangs(function () {
-          $a.siblings('pre').show().animate({ height: "100%" }, 'fast', 'easeOutQuad', function () {
+  Respondo.inits = {
 
-          })
+    languages: function () {
+      var isClosing = false;
+      function closeLangs(cb) {
+        isClosing = true;
+        $('.language-wrap pre').each(function (i, a) {
+          var $a = $(a);
+          $a.animate({ height: 0 }, 'fast', 'easeOutQuad', function () {
+            $a.hide();
+            isClosing = false;
+            cb();
+          });
         });
       }
-    });
-  });
+  
+      $('.language-wrap h3').each(function (i, a) { 
+        var $a = $(a);
+        $a.click(function () {
+          if (!isClosing) {
+            closeLangs(function () {
+              $a.siblings('pre').show().animate({ height: "100%" }, 'fast', 'easeOutQuad', function () {
+
+              })
+            });
+          }
+        });
+      });
+    },
+    content: function () {
+      $('#delete-menu-item a').click(function (e) {
+        if (!confirm('Are you sure you want to delete item?')) {
+          return false;
+        }
+      })
+    }
+  }
+  
+  
+  for (var fn in Respondo.inits) {
+    Respondo.inits[fn]();
+  }
+  
 });
